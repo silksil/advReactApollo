7523901c212527284326f01b1ebd5c16df6b2d1c
Sil <silkreulen@gmail.com>
2019-01-11 15:32:26 +0100

    update auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middlware to handle cookies (JWT)
+server.express.use(cookieParser());
 // TODO Use express middlware to populate current user
 
 server.start(
@@ -15,6 +19,6 @@ server.start(
     },
   },
   deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
-)
+);
